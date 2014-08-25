1275e779d1aa63319eaa7ad680b5fe0946e92d14
Radoslav Stankov <rstankov@gmail.com>
2014-08-25 20:34:57 +0300

    Show “current user”

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,1 +1,12 @@
 window.app = angular.module('Demo', ['ui.router']);
+
+app.value('_', _);
+
+app.value('User', {
+  name: 'Radoslav Stankov'
+});
+
+app.run(function($rootScope, User) {
+  $rootScope.user = User;
+});
+
